954a514280a38f02d85f9ae92af57daf15f4fe13
martinlgalvan <martinlgalvan@yahoo.com>
2025-04-29 04:17:41 -0300

    delete error index.js

--- api/index.js
+++ api/index.js
@@ -2,9 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import WeekApiRoute from './routes/week.api.routes.js';
 import UsersApiRoute from './routes/users.api.routes.js';
-import ExercisesApiRoute from './routes/exercises.api.routes.js';
-
-
 
 const app = express();
 app.use(cors());
@@ -13,7 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', UsersApiRoute);
 app.use('/', WeekApiRoute);
-app.use('/', ExercisesApiRoute);
 
 
 export default app;
